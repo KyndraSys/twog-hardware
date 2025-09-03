a71701b9d6c5ea6243c8ef73f4ad858fe5239a55
KyndraSys <kyndrasystems@gmail.com>
2025-09-03 19:54:23 +0300

    readt for Render

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
-const dotenv = require('dotenv');
+   const dotenv = require('dotenv');
 
-dotenv.config();
+   dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+   const PORT = process.env.PORT || 5000;
+   app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,20 +1,3 @@
-// const { Pool } = require('pg');
-// const dotenv = require('dotenv');
-
-// dotenv.config();
-
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT,
-// });
-
-// module.exports = {
-//   query: (text, params) => pool.query(text, params),
-// };
-
 const { Pool } = require('pg');
 const dotenv = require('dotenv');
 
